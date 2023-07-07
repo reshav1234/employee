041227fc2f602ede9865b4fdb2c8296157d52145
Reshav <thapareshav50@gmail.com>
2023-07-07 22:11:54 +0545

    employee performance display: update

--- client/src/Employee/EmployeeCard.jsx
+++ client/src/Employee/EmployeeCard.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
 
-const EmployeeCard = ({ emp, kpis, handleKPIChange }) => {
+
+const EmployeeCard = ({ emp, kpis }) => {
   const { id, name, position, kpiData } = emp;
+
+  const calculateOverallPerformance = () => {
+    const overallPerformance =
+      ((kpiData[0] / 100) * 33.33) +
+      ((kpiData[1] / 100) * 33.33) +
+      ((kpiData[2] / 100) * 33.33);
+
+    return overallPerformance.toFixed(2); 
+  };
 
   return (
     <div>
       <h3>{name}</h3>
       <p>Position: {position}</p>
-      {kpis.map((kpi, index) => (
-        <div key={index}>
-          <label htmlFor={`kpi-${id}-${index}`}>{kpi}: </label>
-          <input
-            type="number"
-            id={`kpi-${id}-${index}`}
-            value={kpiData[index]}
-            onChange={(event) => handleKPIChange(event, index)}
-          />
-        </div>
-      ))}
+      <p>KPIs:</p>
+      <ul>
+        {kpis.map((kpi, index) => (
+          <li key={index}>
+            {kpi}: {kpiData[index]}
+          </li>
+        ))}
+      </ul>
+      <p>
+        Overall Performance: <span>{calculateOverallPerformance()} %</span>
+      </p>
     </div>
   );
 };
--- client/src/Employee/EmployeeList.jsx
+++ client/src/Employee/EmployeeList.jsx
@@ -4,21 +4,14 @@ const EmployeeList = ({kpis, setKpis}) => {
     const employees = [
         { id: 1, name: 'John Doe', position: 'Sales Executive', kpiData: [100, 85, 92] },
         { id: 2, name: 'Reshav', position: 'Marketing Specialist', kpiData: [75, 90, 80] },
-        { id: 2, name: 'Pragyan', position: 'Manager', kpiData: [75, 90, 80] },
-        { id: 2, name: 'Suyog', position: 'HR', kpiData: [75, 90, 80] },
-        { id: 2, name: 'Aarjan', position: 'Lead Sales', kpiData: [75, 90, 80] },
+        { id: 3, name: 'Pragyan', position: 'Manager', kpiData: [75, 90, 80] },
+        { id: 4, name: 'Suyog', position: 'HR', kpiData: [75, 90, 80] },
+        { id: 5, name: 'Aarjan', position: 'Lead Sales', kpiData: [75, 90, 80] },
       ];
-
-    const handleKPIChange = (event, index) => {
-        const newKpis = [...kpis] 
-        newKpis[index] = event.target.value;
-        setKpis(newKpis);
-    }
-      
   return (
     <>
-        {employees.map((emp, i) =>(
-            <EmployeeCard key = {i} emp = {emp} kpis={kpis} handleKPIChange = {handleKPIChange}/>
+        {employees.map((emp) =>(
+            <EmployeeCard key={emp.id} emp = {emp} kpis={kpis}/>
         ))}
     </>
   )
